c6419237190addf13826295e1eff75580c0851e5
mahesh <mahesh@nextgengolf.org>
2015-08-06 13:17:50 -0400

    Make it 2 player problem

--- tie_breaker.rb
+++ tie_breaker.rb
@@ -1,8 +1,6 @@
 class TieBreaker
   attr_accessor :handicaps
-  attr_accessor :pars
 
-  def sort_scores(scores)
-    scores
+  def break_tie(p1, p2)
   end
 end
--- tie_breaker_test.rb
+++ tie_breaker_test.rb
@@ -5,12 +5,11 @@ class TieBreakerTest < Minitest::Test
 
   def setup
     @tie_breaker = TieBreaker.new
-    @tie_breaker.pars = [4,5,4,4,3,4,4,3,5,4,4,5,4,3,4,4,4,4]
     @tie_breaker.handicaps = [16,2,18,14,12,6,8,10,4,13,9,1,11,17,15,7,5,3]
   end
 
   def test_return_if_only_one_score
-    assert_equal [], @tie_breaker.sort_scores([])
+    assert_equal [], @tie_breaker.break_tie([], [])
   end
 
 end
